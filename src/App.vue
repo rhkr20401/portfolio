02386ad10f41671cfd8655502f66ae099baf0290
rhkr20401 <rhkr20401@gmail.com>
2026-08-08 20:29:51 +0900

    테일윈드/폰트어썸 설치

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1 class="text-red-500">You did it!</h1>
+  <font-awesome-icon icon="fa-solid fa-house" />
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
+import './assets/css/style.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHouse, faUser, faGear } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faHouse, faUser, faGear, faGithub)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
